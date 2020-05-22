83f8b663b5235c90b115323388e54c76156e7f2a
Steve Button <steve@pointers-uk.net>
2020-05-22 17:35:25 +0100

    Added stuff for stock lookup and display inline

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+    def self.new_lookup(ticker_symbol)
+        client = IEX::Api::Client.new(
+            publishable_token: Rails.application.credentials.iex_client[:publishable_token],
+            secret_token: Rails.application.credentials.iex_client[:secret_token],
+            endpoint: 'https://sandbox.iexapis.com/v1'
+        )
+        new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    end
 end
